1dd55f3ef80d010febc8a5ccb5251db2adadd222
Muhammad Rehan <exaltconsultant296@gmail.com>
2026-07-08 19:01:13 +0300

    Add premium global exchange theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.jsx";
+import "./styles/ExaltTheme.css";
 import { I18nProvider } from "./i18n";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <I18nProvider>
