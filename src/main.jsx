1c1352237ec0bc792811c7950b7d70b2d0b9498e
Muhammad Rehan <exaltconsultant296@gmail.com>
2026-07-10 16:07:11 +0300

    Fix app import case for Render build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./app.jsx";
 import "./styles/ExaltTheme.css";
 import { I18nProvider } from "./i18n";
 
